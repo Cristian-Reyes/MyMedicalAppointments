332f2df2f21f92ad8d18f279b5411c1411b23402
Cristian_Reyes <ccristianreyesl@gmail.com>
2020-12-01 17:25:53 -0500

    Mensajes de bienvenida

--- Main.java
+++ Main.java
@@ -14,8 +14,12 @@ public class Main
 
     public static void main(String[] args)
     {
+
+        System.out.println("BIENVENIDO A MyMedicalAppointments");
         //Invoca al menú principal
         showMenu();
+
+        System.out.println("Gracias por hacer uso de nuestra app");
 
     }
 }
