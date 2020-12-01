2bc8f7b7fddce0b015020b34a69e9611229cea1c
Cristian_Reyes <ccristianreyesl@gmail.com>
2020-12-01 17:41:35 -0500

    Se crean mensajes por consola al inicio de la app

--- Main.java
+++ Main.java
@@ -14,8 +14,16 @@ public class Main
 
     public static void main(String[] args)
     {
+
+        System.out.println("Bienvenido a HospitalPro");
+
+        System.out.println("lo invitamos a seleccionar el menú correspondiente");
+
         //Invoca al menú principal
         showMenu();
+
+
+        System.out.println("Gracias por su visita, nos vemos en una proxima");
 
     }
 }
